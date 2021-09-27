78474fb6b401b31526bb6275dcb81ce1c3919840
Tommaso Cammelli <tomm.camm@protonmail.ch>
2021-09-27 21:34:00 +0200

    gitignore

---
+++ main.py
@@ -0,0 +1,51 @@
+import time
+import os
+from dotenv import load_dotenv
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+load_dotenv()
+
+driver = webdriver.Firefox()
+driver.get("https://gestioneorari.didattica.unimib.it/PortaleStudentiUnimib/index.php?view=login&include=login"
+           "&from=prenotalezione&from_include=prenotalezione_home&_lang=it")
+
+# Accept privacy slider
+element = driver.find_element_by_class_name("switch")
+element.click()
+
+# Open login page
+element = driver.find_element_by_id("oauth_btn")
+element.click()
+
+time.sleep(2)
+# Fill with login info
+# username
+element = driver.find_element_by_id("username")
+element.clear()
+element.send_keys(os.environ.get("username"))
+
+# password
+element = driver.find_element_by_id("password")
+element.clear()
+element.send_keys(os.environ.get("password"))
+# Login
+element.send_keys(Keys.RETURN)
+
+time.sleep(1)
+# Open book a lesson
+element = driver.find_element_by_link_text("Prenota il tuo posto a lezione")
+element.click()
+
+# Opens "new booking"
+element = driver.find_element_by_partial_link_text("Nuova prenotazione")
+element.click()
+
+
+
+
+# elem.clear()
+# elem.send_keys("pycon")
+# elem.send_keys(Keys.RETURN)
+# assert "" not in driver.page_source
+# driver.close()
